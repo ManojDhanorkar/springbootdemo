71f938a0b74f4ffe72473dee4d3e73a3dcc17914
ManojDhanorkar <manoj.dhanorkar@gmail.com>
2021-07-05 15:34:38 +0530

    Minor changes

---
+++ src/main/resources/k8/create_demo.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+kubectl create -f demo-configmap.yaml
+kubectl create -f demo-deployment.yaml
+kubectl create -f demo-service.yaml
---
+++ src/main/resources/k8/delete_demo.sh
@@ -0,0 +1,4 @@
+#/bin/bash
+kubectl delete -f demo-configmap.yaml
+kubectl delete -f demo-deployment.yaml
+kubectl delete -f demo-service.yaml
